f52e7774defbe7c7b9822371d1751ddf3a739bfb
Ryan12439 <35814800+Ryan12439@users.noreply.github.com>
2022-11-18 15:52:45 -0600

    Add manual leveling points for Ender 3

---
+++ Creality Ender Series/Ender 3 Manual/Point 2.gcode
@@ -0,0 +1,17 @@
+; Creality Ender Series/Ender 3 Manual/Point 2
+; Leveling locations X: [32]
+; Leveling locations Y: [206]
+; Dwell time: 15
+; Speed: 3000
+; Home before calibration: False
+; Home after calibration: False
+; Z height: 5.8
+; Z move height: 8
+; Disable motors at end: False
+
+
+G90
+G1 Z8
+G1 X32 Y206 F3000
+G1 Z5.8
+G4 P15000
---
+++ Ender 3 Manual.py
@@ -0,0 +1,6 @@
+import compile
+
+compile.compile("Creality Ender Series/Ender 3 Manual/Point 1", [32], [36], overwrite=True, home=False, homeAtEnd=False, disableMotors=False)
+compile.compile("Creality Ender Series/Ender 3 Manual/Point 2", [32], [206], overwrite=True, home=False, homeAtEnd=False, disableMotors=False)
+compile.compile("Creality Ender Series/Ender 3 Manual/Point 3", [202], [206], overwrite=True, home=False, homeAtEnd=False, disableMotors=False)
+compile.compile("Creality Ender Series/Ender 3 Manual/Point 4", [202], [36], overwrite=True, home=False, homeAtEnd=False, disableMotors=False)
